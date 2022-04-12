bcd168cbfba7be8d0fc7465b30ca8148df03b1f2
priscila andreani <priscilaandreani@gmail.com>
2022-04-12 20:39:29 -0300

    feat: add header and footer

---
+++ components/Footer/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Imersão front-end expert</p>
+    </footer>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { Header } from '@/components';
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Header />
+
       <h1 className='underline decoration-dotted'>hello world</h1>
     </div>
   );
