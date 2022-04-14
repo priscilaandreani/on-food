7913278f35b24420b9a34eed94609396c63d7d0f
priscila andreani <priscilaandreani@gmail.com>
2022-04-14 19:09:14 -0300

    feat: add icons to root

---
+++ icons/index.ts
@@ -0,0 +1,19 @@
+import IconBookmark from '../public/images/icon/bookmark.svg';
+import IconCategory from '../public/images/icon/category.svg';
+import IconDelivery from '../public/images/icon/delivery.svg';
+import IconFilter from '../public/images/icon/filter.svg';
+import IconHeart from '../public/images/icon/heart.svg';
+import IconHome from '../public/images/icon/home.svg';
+import IconRestaurant from '../public/images/icon/restaurant.svg';
+import IconReviews from '../public/images/icon/reviews.svg';
+
+export {
+  IconBookmark,
+  IconCategory,
+  IconDelivery,
+  IconFilter,
+  IconHeart,
+  IconHome,
+  IconRestaurant,
+  IconReviews,
+};
