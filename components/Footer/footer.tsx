8a557c2cb22b2dedfc41b7ae50700712d243d109
priscila andreani <priscilaandreani@gmail.com>
2022-04-14 19:10:33 -0300

    feat: add footer

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,7 +1,11 @@
+import Styles from './footer.module.css';
+
 export function Footer() {
   return (
     <footer>
-      <p>Imersão front-end expert</p>
+      <div className={Styles.footer}>
+        <p>@ 2022 - Imersão FrontExpert</p>
+      </div>
     </footer>
   );
 }
